b1bb2a5367a4d8bab73333f2781d5b8e1230e2a7
facugon <facugon@theeye.io>
2021-06-03 17:59:16 -0300

    remove unused code

--- index.js
+++ index.js
@@ -39,19 +39,6 @@ class Api {
       res.status(200).json('ok')
     })
 
-    app.get('/remote', function (req, res) {
-      console.log(req.query)
-      const payload = [
-        { id: 'AbcA1234', label: 'A' },
-        { id: 'AbcB1234', label: 'B' },
-        { id: 'AbcC1234', label: 'C' },
-        { id: 'AbcD1234', label: 'D' },
-        { id: 'AbcE1234', label: 'E' },
-      ]
-
-      res.status(200).json(payload)
-    })
-
     app.listen(8001, function () {
       console.log('listening on port 8001')
     })
